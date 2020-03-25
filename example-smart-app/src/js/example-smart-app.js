63ec4ea86892fcae77d381ca638a927608064529
Usman Amir <usman.amir@aciano.net>
2020-03-25 14:55:28 +0500

    chng

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -82,8 +82,8 @@
                     var cp = smart.patient.api.fetchAll({
                         type: 'CarePlan',
                         query: {
-                            patient: patient.id,
-                            category: 'assess-plan'
+                            patient: patient.id
+                            //,category: 'assess-plan'
                         }
                     });
 
